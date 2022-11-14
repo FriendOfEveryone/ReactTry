95e86a7b9ee68f703fc7dbe50e3e5d25bd927cab
DenisOle <93873674+RusMen007@users.noreply.github.com>
2022-11-14 13:41:02 +0300

    First React App

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://avatars.mds.yandex.net/i?id=fec06be7213d1259d960cde8543a987ee556cbf2-4698303-images-thumbs&n=13"></img>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/Components/Profile.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <img src="https://avatars.mds.yandex.net/i?id=49d294f235a495e954bf0d85aecef5c5-4078450-images-thumbs&n=13"></img>
+            <div>ava + description</div>
+            <div>
+                My posts
+                <div>New post</div>
+                <div>post 1</div>
+                <div>post 2</div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
